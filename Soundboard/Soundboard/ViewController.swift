8ac9fe71b89cdb5fb62ce56b03b6c3998202b2d6
Brent Scarbrough <BrentScarbrough@mau5cy5l72.maurices.com>
2017-12-28 14:28:59 -0500

    Initial

---
+++ Soundboard/Soundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soundboard
+//
+//  Created by Brent Scarbrough on 12/28/17.
+//  Copyright © 2017 Brent Scarbrough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
